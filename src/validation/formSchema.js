112db23dd1491b10004a0d36a525d541e0055261
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-09-22 19:56:39 -0700

    updated schema

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -11,6 +11,9 @@ const formSchema = yup.object().shape({
     sauce: yup
         .string()
         .oneOf(['Original Red', 'Garlic Ranch', 'BBQ Sauce', 'Spinach Alfredo'], 'You must choose a sauce'),
+    pepperoni: yup.boolean(),
+    sausage: yup.boolean(),
+    canadianBacon: yup.boolean(),
     // TOPPINGS... something like: yup.array(yup.boolean()).compact().min(1).max(3)
     // should include each topping separately
     comments: yup
